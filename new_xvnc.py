5c9ab68efc5f814ebea7d586a3e799bd472f206c
cwheyer <cwheyer@umich.edu>
2013-09-17 15:34:49 -0400

    Started new version of xvnc

---
+++ new_xvnc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os, sys, time
+from datetime import timedelta
+
+class 
+
+################################################################################
+# Main Program
+
+for line in sys.stdin.readlines():
