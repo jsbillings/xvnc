fdf5076d3f517fabf3bb50a018efaf074ecdc226
cwheyer <cwheyer@umich.edu>
2013-10-01 11:01:59 -0400

    Finished standard output formatting for the xvnc report generator

--- xvnc.py
+++ xvnc.py
@@ -13,8 +13,11 @@
 #   Christopher W. Heyer (cwheyer@umich.edu): September 2013
 #    
 ################################################################################
-import os, sys, time, re, datetime
+# import modules
+import os, sys, time, re, datetime, argparse
 
+################################################################################
+# XVNC report generator class
 class xvnc_Generator(object):
     regEx = re.compile('(?P<month>\w+) (?P<day>\d+) (?P<logtime>\d\d:\d\d:\d\d) (?P<host>caen-vnc[^.]+\.engin\.umich\.edu) sshd\[(?P<pid>\d+)\]: pam_unix\(sshd:session\): session (?P<action>\w+) for user (?P<user>\w+)')
 
@@ -24,18 +27,10 @@ class xvnc_Generator(object):
         self.start = 0
         self.end = 0
 
-# readLine():
-#   Searches the given line for a valid ssh session log and if successfull, it
-# calls the addExp member function to add this ssh session to the data members
     def readLine(self, line):
         m = self.regEx.search(line)
         if m: self.addExp(m)
-
-# addExp():
-#   Takes a the given valid ssh session log, parses it for the logtime, host,
-# user, pid, and action(open or close an ssh session) and adds it to the login
-# and user data dictionaries through openSess, closeSess, and addUser member
-# functions. 
+ 
     def addExp(self, m):
         month = m.group('month')
         day = m.group('day')
@@ -65,10 +60,6 @@ class xvnc_Generator(object):
             if self.closeSess(host, pid, timeEp):
                 self.addUser(user)
 
-# openSess():
-#   Adds the valid open ssh session log to the list of ssh sessions. If the
-# given log represents a new session that was not already in the login data it
-# returns true, otherwise it returns false.
     def openSess(self, host, pid, timeEp):
         if not self.logins.has_key(host):
             self.logins[host] = {'pids': {}, 'sessions': 0, 'totTime': 0}
@@ -87,11 +78,6 @@ class xvnc_Generator(object):
             self.logins[host]['sessions'] += 1
         return True;
 
-# closeSess():
-#   closeSess() uses the given close ssh session log to remove active sessions
-# and tally total login time for the corresponding host. If the given log 
-# represents a new session that was not already in the login data it returns
-# true, otherwise it returns false.
     def closeSess(self, host, pid, timeEp):
         if not self.logins.has_key(host):
             self.logins[host] = {'pids': {}, 'sessions': 0, 'totTime': 0}
@@ -110,19 +96,12 @@ class xvnc_Generator(object):
             return False;
         return True;
 
-# addUser():
-#   addUser() adds a new key for any unique user not already in the user
-# dictionary and sets it session count to 1. If the user is not unique then it
-# increments the session count by 1.
     def addUser(self, user):
         if not self.users.has_key(user):
             self.users[user] = 1;
         else:
             self.users[user] += 1
 
-# closeLogs():
-#   Closes all the active sessions in the login data dictionary and adds the 
-# total time the session was active up until the end of the report logs.
     def closeLogs(self):
         for host in self.logins.iterkeys():
             for pid in self.logins[host]['pids'].iterkeys():
@@ -133,10 +112,8 @@ class xvnc_Generator(object):
                         item['end'] = self.end
 
 ################################################################################
-
+# Helpful secs to HH:MM:SS function
 def secs2HHMMSS( seconds ):
-  """ converts integer seconds into a time string, HH:MM:SS.
-  """
   hrs = seconds / 3600
   secs = seconds  % 3600
   mins = secs / 60
@@ -155,12 +132,11 @@ reports.closeLogs()
 
 print '\n Report Log Summary:\n'
 print ' %-39.39s %38.38s' % (('Start: %s' % time.asctime(time.localtime(reports.start))), ('End: %s' % time.asctime(time.localtime(reports.end))))
-print ' %s ' % ('-' * 78)
-print
-print '\tHost                 Sessions Total Login Time Avg Login Time'
-print '\t-------------------- -------- ---------------- --------------'
+print ' %s \n' % ('-' * 78)
+print ' Host                                 Sessions Total Login Time Avg Login Time'
+print ' ------------------------------------ -------- ---------------- --------------'
 for host in sorted(reports.logins.keys()):
-    print '\t%-20.20s %8d %16.16s %14.14s' % (host, reports.logins[host]['sessions'], secs2HHMMSS(reports.logins[host]['totTime']), secs2HHMMSS(reports.logins[host]['totTime']/float(reports.logins[host]['sessions'])))
+    print ' %-36.36s %8d %16.16s %14.14s' % (host, reports.logins[host]['sessions'], secs2HHMMSS(reports.logins[host]['totTime']), secs2HHMMSS(reports.logins[host]['totTime']/float(reports.logins[host]['sessions'])))
 print
 print
 
@@ -170,4 +146,4 @@ for user in reports.users.iterkeys():
     numSess += reports.users[user]
 
 print '\tTotal number of unique users: %d\n\tTotal number of sessions: %d\n' % (len(reports.users.keys()), numSess)
-print ' %s ' % ('-' * 78)
+print ' %s \n' % ('-' * 78)
